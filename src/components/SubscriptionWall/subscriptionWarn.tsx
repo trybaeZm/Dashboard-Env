1836a4dac5d942155a7ee5c461be846869f74a47
joshu-inf <jsibanda407@gmail.com>
2025-10-10 23:11:16 +0200

    ok

--- src/components/SubscriptionWall/subscriptionWarn.tsx
+++ src/components/SubscriptionWall/subscriptionWarn.tsx
@@ -2,6 +2,7 @@
 import { getData } from "@/lib/createCookie"
 import { checkSub } from "@/services/subscription/subscriptionService"
 import Cookies from "js-cookie"
+import Link from "next/link"
 import { useEffect, useState } from "react"
 
 export const ShouldShowSubscriptionWarn = () => {
@@ -27,7 +28,11 @@ export const ShouldShowSubscriptionWarn = () => {
         <>
             {compShowAdd === 'show' ?
                 <div className=" w-full h-16 flex justify-between px-10 pointer-events-none text-md  dark:text-gray-300 flex items-center justify-center bg-yellow-100/50 dark:bg-yellow-800/20 rounded-md mb-4 font-medium">
+                    <div>
                     you currently do not have a subscription. please subscribe to continue using the service.
+                    <Link href="" >Subscribe</Link>
+                    </div>
+
                     <button onClick={()=> setcompShowAdd('hide')}>x</button>
                 </div>
                 :
